9425470479a2cd0a0e5be547e9b2175b3758e44d
Kevin Chang <kevinjchang98@gmail.com>
2022-11-01 15:51:53 -0700

    add ScoreGraph tests

---
+++ __tests__/ScoreGraph.test.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import ScoreGraph from '../app/ScoreGraph';
+import '@testing-library/jest-dom';
+
+describe('ScoreGraph', () => {
+    it('renders a graph with valid data', () => {
+        const data = [0, 0, 0, 0];
+
+        const { container } = render(<ScoreGraph data={data} />);
+
+        expect(container.getElementsByClassName('chartContainer').length).toBe(
+            1
+        );
+    });
+
+    it('renders null with invalid data', () => {
+        const { container } = render(<ScoreGraph data={null} />);
+
+        expect(container.getElementsByClassName('chartContainer').length).toBe(
+            0
+        );
+    });
+});
